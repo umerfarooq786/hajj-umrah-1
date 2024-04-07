fb35978ac8b1fb104badecaa4ee86e4b8d7cbcd4
Your Name~ <you@example.com>
2024-04-07 19:57:57 +0500

    ...

--- resources/views/admin_layouts/partials/_fixed_top.blade.php
+++ resources/views/admin_layouts/partials/_fixed_top.blade.php
@@ -11,9 +11,9 @@
                         <h3 class="brand-text">Fast Lines</h3>
                     </a>
                 </li>
-                <li class="nav-item d-none d-md-block float-right"><a class="nav-link modern-nav-toggle pr-0"
+                {{-- <li class="nav-item d-none d-md-block float-right"><a class="nav-link modern-nav-toggle pr-0"
                         data-toggle="collapse"><i class="toggle-icon ft-toggle-right font-medium-3 white"
-                            data-ticon="ft-toggle-right"></i></a></li>
+                            data-ticon="ft-toggle-right"></i></a></li> --}}
                 <li class="nav-item d-md-none">
                     <a class="nav-link open-navbar-container" data-toggle="collapse" data-target="#navbar-mobile"><i
                             class="la la-ellipsis-v"></i></a>
--- resources/views/pdf/pdfDocument.blade.php
+++ resources/views/pdf/pdfDocument.blade.php
@@ -7,7 +7,101 @@
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Invoice</title>
-    <link rel="stylesheet" href="{{ asset('css/pdf.css') }}" type="text/css">
+    {{-- <link rel="stylesheet" href="{{ asset('css/pdf.css') }}" type="text/css"> --}}
+    <style>
+        .invoice-box {
+            max-width: 800px;
+            margin: auto;
+            padding: 30px;
+            border: 1px solid #eee;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
+            font-size: 16px;
+            line-height: 24px;
+            font-family: "Helvetica Neue", "Helvetica", Helvetica, Arial, sans-serif;
+            color: #555;
+        }
+
+        .invoice-box table {
+            width: 100%;
+            line-height: inherit;
+            text-align: left;
+        }
+
+        .invoice-box table td {
+            padding: 5px;
+            vertical-align: top;
+        }
+
+        .invoice-box table tr td:nth-child(2) {
+            text-align: right;
+        }
+
+        .invoice-box table tr.top table td {
+            padding-bottom: 20px;
+        }
+
+        .invoice-box table tr.top table td.title {
+            font-size: 45px;
+            line-height: 45px;
+            color: #333;
+        }
+
+        .invoice-box table tr.information table td {
+            padding-bottom: 40px;
+        }
+
+        .invoice-box table tr.heading td {
+            background: #eee;
+            border-bottom: 1px solid #ddd;
+            font-weight: bold;
+        }
+
+        .invoice-box table tr.details td {
+            padding-bottom: 20px;
+        }
+
+        .invoice-box table tr.item td {
+            border-bottom: 1px solid #eee;
+        }
+
+        .invoice-box table tr.item.last td {
+            border-bottom: none;
+        }
+
+        .invoice-box table tr.total td:nth-child(2) {
+            border-top: 2px solid #eee;
+            font-weight: bold;
+        }
+
+        @media only screen and (max-width: 600px) {
+            .invoice-box table tr.top table td {
+                width: 100%;
+                display: block;
+                text-align: center;
+            }
+
+            .invoice-box table tr.information table td {
+                width: 100%;
+                display: block;
+                text-align: center;
+            }
+        }
+
+        /** RTL **/
+        .invoice-box.rtl {
+            direction: rtl;
+            font-family: Tahoma, "Helvetica Neue", "Helvetica", Helvetica, Arial,
+                sans-serif;
+        }
+
+        .invoice-box.rtl table {
+            text-align: right;
+        }
+
+        .invoice-box.rtl table tr td:nth-child(2) {
+            text-align: left;
+        }
+    </style>
 </head>
 
 <body>
@@ -18,7 +112,10 @@
                     <table>
                         <tr>
                             <td class="title">
-                                <img src="{{ asset('images/logo.png') }}" style="width: 100%; max-width: 200px" />
+                                {{-- <img src="{{ asset('images/logo.png') }}" style="width: 100%; max-width: 200px" /> --}}
+                                <img src="https://res.cloudinary.com/ddxbvngpc/image/upload/v1712501587/nej3hzcul3cenxlqkfnp.png"
+                                    style="width: 100%; max-width: 200px" />
+
                             </td>
 
                             <td>
