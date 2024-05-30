ad9fde30c7330c3fa3727f794959d0cf7cc6f4c0
MudasirTech786 <m.mudasirasfi@gmail.com>
2024-05-30 17:59:47 +0500

    --

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('process:hotel-validity-notifications')->everyMinute();
         $schedule->command('process:transport-validity')->everyMinute();
     }
 
